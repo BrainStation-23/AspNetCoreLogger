bdac3111f5d5f9a32d28f3ad5d4b3e0ebb016c3e
Md Kakuya Taslim <mdtaslim1@yahoo.com>
2022-06-25 18:39:27 +0600

    json serialize with test

--- WebApp.Core.Test/Serializers/JsonSerializeExtensionTests.cs
+++ WebApp.Core.Test/Serializers/JsonSerializeExtensionTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using WebApp.Common.Serialize;
 using WebApp.Core.Files;
 
@@ -20,6 +21,53 @@ namespace WebApp.Core.Test.Serializers
 
 
             var actual = escapeJsonString.JsonUnescaping();
+
+            Assert.AreEqual(actual, unescapeFileString);
+        }
+
+        [TestMethod]
+        public void JsonSerializeExtensionTests_AuditJson()
+        {
+            var escapeJsonPath = "./Jsons/audit-escape-json.json";
+            var escapeJsonString = escapeJsonPath.ReadFileAsText();
+
+            var unescapeFilePath = "./Jsons/audit-unescape-json.txt";
+            var unescapeFileString = unescapeFilePath.ReadFileAsText();
+
+
+            var actual = escapeJsonString.JsonUnescaping();
+            var parsed = JArray.Parse(actual);
+
+            Assert.AreEqual(actual, unescapeFileString);
+        }
+
+        [TestMethod]
+        public void JsonSerializeExtensionTests_RouteJson()
+        {
+            var escapeJsonPath = "./Jsons/route-escape-json.json";
+            var escapeJsonString = escapeJsonPath.ReadFileAsText();
+
+            var unescapeFilePath = "./Jsons/route-unescape-json.txt";
+            var unescapeFileString = unescapeFilePath.ReadFileAsText();
+
+
+            var actual = escapeJsonString.JsonUnescaping();
+            var parsed = JArray.Parse(actual);
+
+            Assert.AreEqual(actual, unescapeFileString);
+        }
+
+        [TestMethod]
+        public void JsonSerializeExtensionTests_ErrorJson()
+        {
+            var escapeJsonPath = "./Jsons/error-escape-json.json";
+            var escapeJsonString = escapeJsonPath.ReadFileAsText();
+
+            var unescapeFilePath = "./Jsons/error-unescape-json.txt";
+            var unescapeFileString = unescapeFilePath.ReadFileAsText();
+
+            var actual = escapeJsonString.JsonUnescaping();
+            var parsed = JArray.Parse(actual);
 
             Assert.AreEqual(actual, unescapeFileString);
         }
