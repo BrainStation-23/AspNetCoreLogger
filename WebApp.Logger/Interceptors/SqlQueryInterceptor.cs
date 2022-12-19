cd1a206f69ac2b24ad221fc096cbd54436ad0098
BS1207 <refatm452@gmail.com>
2022-12-19 16:35:50 +0600

    implement-sql-logs-based-on-configuration

--- WebApp.Logger/Interceptors/SqlQueryInterceptor.cs
+++ WebApp.Logger/Interceptors/SqlQueryInterceptor.cs
@@ -18,16 +18,12 @@ namespace WebApp.Logger.Interceptors
     {
         private readonly IHttpContextAccessor Context;
         private readonly ISqlLogRepository SqlLogRepository;
-        private readonly LogOption _logOption;
-     
+
         public SqlQueryInterceptor(IHttpContextAccessor context,
-            ISqlLogRepository sqlLogRepository,
-           
-            IOptions<LogOption> logOption)
+            ISqlLogRepository sqlLogRepository)
         {
             Context = context;
             SqlLogRepository = sqlLogRepository;
-            _logOption = logOption.Value;
         }
 
         public override DbDataReader ReaderExecuted(DbCommand command,
@@ -78,8 +74,8 @@ namespace WebApp.Logger.Interceptors
 
         private async Task ManipulateCommandAsync(DbCommand command, CommandExecutedEventData commandExecutedEventData)
         {
-            if (_logOption.LogType.Contains(LogType.Sql.ToString()))
-                return;
+            //if (_logOption.LogType.Contains(LogType.Sql.ToString()))
+            //    return;
 
             var context = Context.HttpContext;
             var model = new SqlModel
--- WebApp.Logger/Loggers/AuditLogContext.cs
+++ WebApp.Logger/Loggers/AuditLogContext.cs
@@ -4,14 +4,15 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
+using System.Data;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using WebApp.Common.Contexts;
 using WebApp.Core.Acls;
-using WebApp.Logger.Extensions;
 using WebApp.Logger.Interceptors;
 using WebApp.Logger.Loggers;
 using WebApp.Logger.Loggers.Repositories;
@@ -30,19 +31,17 @@ namespace WebApp.Sql
     {
         public readonly ISignInHelper _signInHelper;
         public readonly IAuditLogRepository _auditLogRepository;
+        private readonly ISqlLogRepository _sqlLogRepository;
         public readonly IHttpContextAccessor HttpContextAccessor;
         private long? UserId;
+        private ILoggerFactory _myLoggerFactory;
         private readonly IServiceProvider _serviceProvider;
         private readonly IConfiguration _configuration;
-        private readonly LogOption _logOption;
-        private readonly ISqlLogRepository SqlLogRepository;
-        
+        private readonly LogOption _logOptions;
 
         public AuditLogContext(DbContextOptions options,
             IConfiguration configuration,
-            IServiceProvider serviceProvider,
-            ISqlLogRepository sqlLogRepository,
-            IOptions<LogOption> logOption) : base(options)
+            IServiceProvider serviceProvider) : base(options)
         {
             _serviceProvider = serviceProvider;
             _configuration = configuration;
@@ -50,18 +49,28 @@ namespace WebApp.Sql
             _auditLogRepository = _serviceProvider.GetService<IAuditLogRepository>();
             HttpContextAccessor = _serviceProvider.GetService<IHttpContextAccessor>();
             UserId = _signInHelper.UserId;
-            SqlLogRepository= sqlLogRepository;
+            _sqlLogRepository = _serviceProvider.GetService<ISqlLogRepository>();
             //var logOption = new LogOption();
-            //_logOption = _configuration.GetSection(LogOption.Name).Get<LogOption>();
-            _logOption = logOption.Value;
+            _logOptions = configuration.GetSection(LogOption.Name).Get<LogOption>();
         }
 
         protected AuditLogContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(Console.WriteLine);
+            optionsBuilder.LogTo(message => LoggerExtension.SqlQueryLog(message));
+            optionsBuilder.AddInterceptors(new SqlQueryInterceptor(HttpContextAccessor, _sqlLogRepository));
+            //optionsBuilder.AddInterceptors(new SqlSaveChangesInterceptor(HttpContextAccessor, _sqlLogRepository, _logOptions));
+            //optionsBuilder.AddInterceptors(new SqlTransactionInterceptor(HttpContextAccessor, _sqlLogRepository, _logOptions));
+            optionsBuilder.UseLoggerFactory(_myLoggerFactory).EnableSensitiveDataLogging();
+        }
 
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             bool hasAuditData = false;
             hasAuditData = await AuditTrailLog();
+
             if (hasAuditData)
                 return await base.SaveChangesAsync(cancellationToken);
 
@@ -77,39 +86,25 @@ namespace WebApp.Sql
             return base.SaveChanges();
         }
 
-        #region Audit Logic
-        private void Audit()
-        {
-            if (_signInHelper.IsAuthenticated)
-                UserId = _signInHelper.UserId;
-
-            base.ChangeTracker.Audit(UserId.Value, _logOption);
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.LogTo(Console.WriteLine);
-            optionsBuilder.LogTo(message => LoggerExtension.SqlQueryLog(message));
-            //optionsBuilder.AddInterceptors(new SqlQueryInterceptor(HttpContextAccessor));
-            optionsBuilder.AddInterceptors(new SqlQueryInterceptor(HttpContextAccessor, SqlLogRepository, _logOption));
-            optionsBuilder.AddInterceptors(new SqlSaveChangesInterceptor(HttpContextAccessor, SqlLogRepository, _logOption));
-            optionsBuilder.AddInterceptors(new SqlTransactionInterceptor(HttpContextAccessor, SqlLogRepository, LogOption));
-            optionsBuilder.UseLoggerFactory(_myLoggerFactory).EnableSensitiveDataLogging();
-        }
+        #region audit logic
         private async Task<bool> AuditTrailLog()
         {
+            //if (!logOptions.LogType.MustContain(LogType.Sql.ToString()))
+            //    skip = true;
+
             long userId = 0;
 
             if (_signInHelper.IsAuthenticated)
                 userId = (long)_signInHelper.UserId;
 
-            var auditEntries = base.ChangeTracker.AuditTrailLog(userId, _logOption);
+            var auditEntries = base.ChangeTracker.AuditTrailLog(userId, _logOptions);
             auditEntries.ToList().ForEach(x => x.TraceId = HttpContextAccessor.HttpContext.TraceIdentifier);
 
             if (auditEntries.Any())
             {
                 var factory = new ProviderFactory(_serviceProvider);
 
-                var providerType = _logOption.ProviderType;
+                var providerType = _logOptions.ProviderType;
                 ILog loggerWrapper = factory.Build(providerType);
 
                 await loggerWrapper.Audit.AddAsync(auditEntries.ToList());
