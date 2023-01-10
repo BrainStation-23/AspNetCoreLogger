4b44c268a53ed9a119e30882e1bf7e08a9c08c65
iMu21 <ik21.cse14@gmail.com>
2023-01-10 11:30:54 +0600

    ObjectExtensionTest for ReadNestedObject function is implemented

--- WebApp.Logger.Test/Extensions/ObjectExtensionTests.cs
+++ WebApp.Logger.Test/Extensions/ObjectExtensionTests.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Azure.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using WebApp.Logger.Extensions;
+using WebApp.Logger.Models;
 using WebApp.Logger.Test.Samples.NestedObject;
 
 namespace WebApp.Logger.Test.Extensions
@@ -9,6 +13,8 @@ namespace WebApp.Logger.Test.Extensions
     public class ObjectExtensionTests
     {
         public MyClass MyClass { get; set; }
+
+        public object NestedObject { get; set; }
 
         [TestInitialize]
         public void Initialize()
@@ -40,15 +46,63 @@ namespace WebApp.Logger.Test.Extensions
             };
         }
 
+        //[TestMethod]
+        //public void ObjectExtensionTests_Iteterate()
+        //{
+        //    var obj = MyClass;
+
+        //    var newObj = obj.Iterate();
+
+        //    Assert.IsNotNull(newObj);
+        //    //Assert.AreEqual(newObj.Name, obj.Name);
+        //}
+
         [TestMethod]
-        public void ObjectExtensionTests_Iteterate()
+        public void ObjectExtensionTests_ReadNestedObject_ErrorObject()
         {
-            var obj = MyClass;
+            var errorObject = ObjectInitExtension.GetErrorObject();
 
-            var newObj = obj.Iterate();
+            var errorData = errorObject.ReadNestedObject();
+            var errorAsHash = errorData as Hashtable;
 
-            Assert.IsNotNull(newObj);
-            //Assert.AreEqual(newObj.Name, obj.Name);
+            Assert.IsNotNull(errorAsHash["host"]);
+            Assert.AreEqual(errorAsHash["version"], "https");
+        }
+
+        [TestMethod]
+        public void ObjectExtensionTests_ReadNestedObject_SqlObject()
+        {
+            var sqlObject = ObjectInitExtension.GetSqlObject();
+
+            var sqlData = sqlObject.ReadNestedObject();
+            var sqlAsHash = sqlData as Hashtable;
+
+            Assert.IsNotNull(sqlAsHash["host"]);
+            Assert.AreEqual(sqlAsHash["traceId"], "400001b9-0000-fc00-b63f-84710c7967bb");
+        }
+
+        [TestMethod]
+        public void ObjectExtensionTests_ReadNestedObject_RequestObject()
+        {
+            var requestObject = ObjectInitExtension.GetRequestObject();
+
+            var requestData = requestObject.ReadNestedObject();
+            var request = requestData as Hashtable;
+
+            Assert.IsNotNull(request["response"]);
+            Assert.AreEqual(request["ipAddress"], "::1");
+        }
+
+        [TestMethod]
+        public void ObjectExtensionTests_ReadNestedObject_AuditObject()
+        {
+            var auditObject = ObjectInitExtension.GetAuditObject();
+
+            var auditData = auditObject.ReadNestedObject();
+            var auditAsHash = auditData as Hashtable;
+
+            Assert.IsNotNull(auditAsHash["type"]);
+            Assert.AreEqual(auditAsHash["traceId"], "400001b9-0000-fc00-b63f-84710c7967bb");
         }
     }
 }
