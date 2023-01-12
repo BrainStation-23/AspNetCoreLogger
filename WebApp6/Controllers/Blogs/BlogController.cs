bd385807507e99555c05b98192a9047122fe6de2
BS1207 <refatm452@gmail.com>
2023-01-12 15:29:44 +0600

    webapp6 created

---
+++ WebApp6/Controllers/Blogs/BlogController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using Swashbuckle.AspNetCore.Filters;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApp.Common.Responses;
+using WebApp.Core;
+using WebApp.Entity.Entities.Blogs;
+using WebApp.Service;
+using WebApp.Service.Contract.Models.Blogs;
+using WebApp6.Helpers.Base;
+using WebApp6.Swaggers.Examples.Requests.Blogs;
+using WebApp6.Swaggers.Examples.Responses.Blogs;
+
+namespace WebApp6.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class BlogController : GenericBaseController<BlogEntity, BlogModel>
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService, IMapper mapper) : base(blogService, mapper)
+        {
+            _blogService = blogService;
+        }
+
+        [HttpGet("search")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string? searchText = null)
+        {
+            var res = await _blogService.GetSearchAsync(pageIndex, pageSize, searchText);
+
+            return new OkResponse(res);
+        }
+
+        [SwaggerRequestExample(typeof(BlogModel), typeof(BlogRequestExample))]
+        [SwaggerResponse(200, Type = typeof(BlogModel), Description = "successful...")]
+        [SwaggerResponseExample(200, typeof(BlogResponseExample))]
+        [SwaggerResponse(404, Type = typeof(BlogModel), Description = "failed...")]
+        [SwaggerResponseExample(404, typeof(BlogResponse404Example))]
+        [ProducesResponseType(typeof(BlogModel), 200)]
+        [HttpPost()]
+        public async Task<IActionResult> AddBlogDetailAsync(BlogModel model)
+        {
+            var res = await _blogService.AddBlogDetailAsync(model);
+
+            return new OkResponse(res);
+        }
+
+        [HttpPut("{blogId}")]
+        public async Task<IActionResult> UpdateBlogDetailAsync(long blogId, [FromForm] BlogModel model)
+        {
+            var res = await _blogService.UpdateBlogDetailAsync(blogId, model);
+
+            return new OkResponse(res);
+        }
+
+        [HttpPost("BulkBlog")]
+        public async Task<IActionResult> BulkBlogAsync(List<BlogBulkModel> blogs)
+        {
+            var res = await _blogService.AddBulkBlogAsync(blogs);
+
+            return new OkResponse(res);
+        }
+
+        [HttpPost("BlogOperation")]
+        public async Task BlogOperationAsync()
+        {
+            await _blogService.AddBlogOperationAsync();
+
+        }
+    }
+}
---
+++ WebApp6/Swaggers/Examples/Responses/Blogs/BlogResponseExample.cs
@@ -0,0 +1,18 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using WebApp.Service.Contract.Models.Blogs;
+
+namespace WebApp6.Swaggers.Examples.Responses.Blogs
+{
+    public class BlogResponseExample : IExamplesProvider<BlogModel>
+    {
+        public BlogModel GetExamples()
+        {
+            return new BlogModel
+            {
+                Name = "My Blog Response",
+                Description = "My blog description",
+                Motto = "Blog Motto"
+            };
+        }
+    }
+}
